22d0975939b1e7888acb3bd0eadb181aa6ef402b
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-05-31 02:17:33 +0200

    attempted adding long spectrum comparison script

--- pysupport/testsame.py
+++ pysupport/testsame.py
@@ -260,5 +260,180 @@ def testsame_shortspectra(filen1, filen2, specnums='all',\
     return (asame and didentical and dsimilar)  
                 
         
+def testsame_longspectra(filen1, filen2, specnums='all',\
+                          name1='file1', name2='file2'):
+    '''
+    test if the specwizard outputs file1 and file2 are the same (for the 
+    selected spectra) 
+    useful to check if debugging had unwanted/unexpected side effects
     
+    input:
+    ------
+    filen1:   name of the first hdf5 output file
+    filen2:   name of the second hdf5 output file
+    specnums: list-like of integers or 'all'; which spectrum numbers to compare
+              number are assumed to match between the files
+    name1:    name for the first file (used in reporting differences)
+    name2:    name for the second file (used in reporting differences)
+    
+    returns:
+    --------
+    True/False: whether all outputs are identical 
+    
+    more detailed differences are printed
+    '''
+    
+    
+    checkpaths_attrs = ['Constants',\
+                        'Header',\
+                        'Header/ModifyMetallicityParameters',\
+                        'Parameters/ChemicalElements',\
+                        'Parameters/SpecWizardRuntimeParameters',\
+                        'Units']
+    checksame_arns = ['Wavelength_Ang']
+    
+    sgrps_rsmass = ['LOSPeculiarVelocity_KMpS',\
+                    'MetalMassFraction',\
+                    'OverDensity',\
+                    'Temperature_K']    
+    sgrps_ion = ['LogTotalIonColumnDensity',\
+                 'RedshiftSpaceOpticalDepthOfStrongestTransition']
+    sgrps_ionw = ['LOSPeculiarVelocity_KMpS',\
+                  'NIon_CM3',\
+                  'OverDensity',\
+                  'Temperature_K']
+    gen_rsnionw = ['NIon_CM3']
+    
+    sgrps_shortspecinfo =['FileUsed',\
+                          'Ibfactor',\
+                          'Icshift',\
+                          'LosUsed',\
+                          'NumberOfShortSpectra',\
+                          'RandomSeeds',\
+                          'x-axis',\
+                          'x_simunits',\
+                          'y-axis',\
+                          'y-simunits',\
+                          'z-axis',\
+                          'z-simunits',\
+                          ]
+    
+    ionw_st = 'RealSpaceNionWeighted'
+    tauw_st = 'RedshiftSpaceOpticalDepthWeighted'
+    
+    asame = True
+    didentical = True
+    dsimilar = True
+    
+    kwfmt = {'f1': name1, 'f2': name2}
+    kw_allclose = {}
+    kwa = {'name1': name1, 'name2': name2, 'kw_allclose': kw_allclose}
+    
+    with h5py.File(filen1, 'r') as f1, h5py.File(filen2, 'r') as f2:
+        # check attributes
+        for path in checkpaths_attrs:
+            gn1 = name1 + ' ' + path
+            gn2 = name2 + ' ' + path
+            asame &= checksame_attrs(f1[path], f2[path], name1=gn1, name2=gn2)
+        # check arrays not specific to spectra
+        for aname in checksame_arns:
+            identical, similar = checksame_arrays(f1, f2, aname, **kwa)
+            didentical &= identical
+            dsimilar &= similar
+        
+        # find and check specnums
+        if specnums == 'all':
+            sks1 = set(f1.keys())
+            sks2 = set(f2.keys())
+            if not sks1 == sks2:
+                print('{f1} and {f2} do not contain the same spectra'.format(**kwfmt))
+                print('{f1} but not {f2}: {k1}'.format(k1=sorted(list(sks1 - sks2)), **kwfmt))
+                print('{f2} but not {f1}: {k2}'.format(k2=sorted(list(sks2 - sks1)), **kwfmt))
+                return False
+            specgroups = set(sk if 'Spectrum' in sk else None for sk in sks1)
+            specgroups -= {None}
+            specgroups = sorted(list(specgroups), key=lambda grn: int(grn[8:]))
+            specnums = np.array([int(grn[8:]) for grn in specgroups])
+        else:
+            specnums = np.array(sorted(list(specnums)))
+            specgroups = ['Spectrum{sn}'.format(sn=sn) for sn in specnums]
+            sks1 = set(f1.keys())
+            sks2 = set(f2.keys())
+            
+            retf = False
+            if not set(specgroups).issubset(sks1):
+                print('{f1} missing requested spectra: {k1}'.format(\
+                      k1=sorted(list(specgroups - sks1)), **kwfmt))
+                retf = True
+            if not set(specgroups).issubset(sks2):
+                print('{f2} missing requested spectra: {k2}'.format(\
+                      k1=sorted(list(specgroups - sks2)), **kwfmt))
+                retf = True
+            if retf:
+                return False
+        
+        # check if output spectra are the same
+        for specgroup in specgroups:
+            g1 = f1[specgroup]
+            g2 = f2[specgroup]
+            asame &= checksame_attrs(f1[path], f2[path],\
+                                     name1=name1 + ' ' + specgroup,\
+                                     name2=name2 + ' ' + specgroup)
+            # loop over subgroups (if different, should be reported in attributes already)
+            lkeys = list(set(g1.keys()) & set(g2.keys()))
+            for ion in lkeys:
+                if ion == 'RealSpaceMassWeighted':
+                    sgrps = sgrps_rsmass
+                    for sgrp in sgrps:
+                        path = '/'.join([specgroup, ion, sgrp])
+                        identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                        didentical &= identical
+                        dsimilar &= similar
+                elif ion == 'Redshift_RealSpace':
+                    path = '/'.join([specgroup, ion])
+                    identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                    didentical &= identical
+                    dsimilar &= similar
+                elif ion == 'Flux':
+                    path = '/'.join([specgroup, ion])
+                    identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                    didentical &= identical
+                    dsimilar &= similar
+                elif ion == 'ShortSpectraInfo':
+                    for sgrp in sgrps_shortspecinfo:
+                        path = '/'.join([specgroup, ion, sgrp])
+                        identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                        didentical &= identical
+                        dsimilar &= similar
+                else:
+                    sgrps = sgrps_ion
+                    for sgrp in sgrps:
+                        path = '/'.join([specgroup, ion, sgrp])
+                        identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                        didentical &= identical
+                        dsimilar &= similar
+                    if ionw_st in g1[ion] and ionw_st in g2[ion]:
+                        for s2grp in sgrps_ionw:
+                            path = '/'.join([specgroup, ion, ionw_st, s2grp])
+                            identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                            didentical &= identical
+                            dsimilar &= similar
+                        for s2grp in gen_rsnionw:
+                            path = '/'.join([specgroup, ion, s2grp])
+                            identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                            didentical &= identical
+                            dsimilar &= similar
+                    if tauw_st in g1[ion] and tauw_st in g2[ion]:
+                        for s2grp in sgrps_ionw:
+                            path = '/'.join([specgroup, ion, tauw_st, s2grp])
+                            identical, similar = checksame_arrays(f1, f2, path, **kwa)
+                            didentical &= identical
+                            dsimilar &= similar
+    if asame:
+        print('All attributes matched')
+    if didentical:
+        print('All arrays were identical')
+    if dsimilar and not didentical:
+        print('All arrays were similar, though at least some were not identical')
+    return (asame and didentical and dsimilar) 
     
